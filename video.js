a920a5e4b98676c3afb9c4d98dca7333d11c445d
agnesmar6314 <144691424+agnesmar6314@users.noreply.github.com>
2023-11-07 21:25:32 -0500

    Add files via upload

---
+++ video.js
@@ -0,0 +1,31 @@
+var video = document.querySelector("video");
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+	video.autoplay = false
+	video.loop = false 
+
+});
+
+ document.querySelector("#play").addEventListener("click", function() {
+ 	console.log("Play Video");
+	video.play();
+ });
+
+ document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+   video.pause();
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	console.log("Video Muted");
+	video.muted = true;
+	slider.min = true;
+});
+
+document.querySelector("#vintage").addEventListener("click", function() {
+	console.log("Old School!");
+	document.querySelector("#vintage").innerHTML
+});
+
+
